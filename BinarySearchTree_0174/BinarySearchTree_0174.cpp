cf209dec29638b50cf53ec02203de7b6bffab3d5
thekill022 <gibfathoni@gmail.com>
2024-06-13 11:23:35 +0700

    membuat dan mendefinisikan prosedur inorder

--- BinarySearchTree_0174/BinarySearchTree_0174.cpp
+++ BinarySearchTree_0174/BinarySearchTree_0174.cpp
@@ -79,6 +79,20 @@ public :
         }
     }
 
+    //membuat dan mendefinisikan prosedur inorder
+    void inorder(Node* ptr) {
+
+        if (ROOT == nullptr) {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != nullptr) {
+            inorder(ptr->leftChild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightChild);
+        }
+    }
+
 };
 
 int main()
