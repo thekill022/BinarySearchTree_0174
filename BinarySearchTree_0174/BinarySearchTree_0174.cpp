6dbb5f52e59d5d309da9e5c405b64b36865f37e2
thekill022 <gibfathoni@gmail.com>
2024-06-13 10:46:04 +0700

    memberi akses modifier serta mendeklarasi variabel dan object node

--- BinarySearchTree_0174/BinarySearchTree_0174.cpp
+++ BinarySearchTree_0174/BinarySearchTree_0174.cpp
@@ -4,6 +4,13 @@ using namespace std;
 //membuat class node
 class Node {
 
+    //memberi akses modifier serta mendeklarasi variabel dan object node
+public :
+    string info;
+    Node* leftChild;
+    Node* rightChild;
+
+
 };
 
 int main()
