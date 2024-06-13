2bd52f4b1e5f4194e653f508d977df8d261ce442
thekill022 <gibfathoni@gmail.com>
2024-06-13 11:48:34 +0700

    membuat conditional statement dari input ch

--- BinarySearchTree_0174/BinarySearchTree_0174.cpp
+++ BinarySearchTree_0174/BinarySearchTree_0174.cpp
@@ -146,6 +146,38 @@ int main()
         cin >> ch;
         cout << endl;
 
+        //membuat conditional statement dari input ch
+        switch (ch)
+        {
+        case '1':
+        {
+            cout << "Enter a word : ";
+            string word;
+            cin >> word;
+            x.insert(word);
+            break;
+        }  
+        case '2': {
+            x.inorder(x.ROOT);
+            break;
+        }    
+        case '3': {
+            x.preorder(x.ROOT);
+            break;
+        }     
+        case '4': {
+            x.postorder(x.ROOT);
+            break;
+        }
+        case '5': {
+            return 0;
+        }
+        default: {
+            cout << "Invalid Option" << endl;
+            break;
+        }
+        }
+
     }
 
 }
